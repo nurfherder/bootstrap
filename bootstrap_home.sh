38012922d7c8578d1382a7074e6a950efdb840cb
nurfherder23 <nurfherder23@gmail.com>
2020-02-26 10:28:44 -0600

    feat: add switch for using git repos via https

--- bootstrap_home.sh
+++ bootstrap_home.sh
@@ -2,11 +2,21 @@
 #
 # Bootstrap my home directory
 #
+
+# Use 'git' or 'https'
+git_proto='git'
+
 git_user='nurfherder'
 git_origin='github.com'
 iws=$(pwd -P)
-myrepo="git://${git_origin}/${git_user}"
-code_path="code/${git_origin}/${git_user}"
+
+if [ "${git_proto}" -eq 'git' ]; then
+  myrepo="git://git@${git_origin}/${git_user}"
+else
+  myrepo="https://${git_origin}/${git_user}"
+fi
+
+code_path="${HOME}/code/${git_origin}/${git_user}"
 
 #---------------------------------------
 # slink
@@ -15,33 +25,34 @@ code_path="code/${git_origin}/${git_user}"
 # create all the symlinks into the git
 # projects.
 #---------------------------------------
-if [ ! -d "$HOME/${code_path}/slink" ]; then
-  git clone "$myrepo/slink.git" "$HOME/${code_path}/slink"
-  cd "$HOME/${code_path}/slink"
+if [ ! -d "${code_path}/slink" ]; then
+  git clone "$myrepo/slink.git" "${code_path}/slink"
+  cd "${code_path}/slink"
 else
-  cd "$HOME/${code_path}/slink"
-  if [ -d "$HOME/code/github/slink.git" ]; then
+  cd "${code_path}/slink"
+  if [ -d "${code_path}/slink/.git" ]; then
     git pull origin master
   fi
 fi
 ./slink
 
 #---------------------------------------
-# Setup code from github
+# Setup other projects from github
 #
 # Requires: git, slink
 #---------------------------------------
-projects='dotshell dottmux dotvim dotruby newbourne'
+projects='code dotshell dottmux dotvim dotruby dothammerspoon newbourne'
 for prj in $projects; do
-  if [ ! -d "$HOME/${code_path}/$prj" ]; then
-    git clone "$myrepo/${prj}.git" "$HOME/${code_path}/$prj"
-    cd "$HOME/${code_path}/$prj"
+  if [ ! -d "${code_path}/$prj" ]; then
+    git clone "$myrepo/${prj}.git" "${code_path}/$prj"
+    cd "${code_path}/$prj"
     if [ -s ./.gitmodules ]; then
       git submodule update --init
     fi
   else
-    cd "$HOME/${code_path}/$prj"
-    if [ -d "$HOME/${code_path}/$prj/.git" ]; then
+    cd "${code_path}/$prj"
+    if [ -d "${code_path}/$prj/.git" ]; then
+      git pull origin master
       git submodule foreach git pull origin master
     fi
   fi
