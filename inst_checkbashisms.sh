e2fa41d4172c2de711df4ba8aeb1c26933903ebe
nurfherder23 <nurfherder23@gmail.com>
2020-02-26 13:21:25 -0600

    feat: use newest version of checkbashisms

--- inst_checkbashisms.sh
+++ inst_checkbashisms.sh
@@ -10,13 +10,13 @@
 #---------------------------------------
 if [ ! -f "$HOME/bin/checkbashisms" ]; then
   echo " * Checkbashisms installing"
-  version='devscripts_2.12.6+deb7u2'
-  version_ALT='devscripts-2.12.6+deb7u2' #CUTE: change _ to -
-  archive="${version}.tar.gz"
+  version='devscripts_2.20.2'
+  version_ALT='devscripts-2.20.2' #CUTE: change _ to -
+  archive="${version}.tar.xz"
   file="${version_ALT}/scripts/checkbashisms.pl"
   cd /tmp
   eval wget http://ftp.de.debian.org/debian/pool/main/d/devscripts/${archive}
-  eval tar -zxf ${archive} ${file}
+  eval tar -Jxf ${archive} ${file}
   eval mv ${file} ~/bin/checkbashisms  #strip '.pl' for syntastic
   eval rm -f ${archive}
   eval rm -rf ${version_ALT}
