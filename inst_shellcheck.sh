100f6adcdfa0635cb10660177b90d34657f3ce18
nurfherder23 <nurfherder23@gmail.com>
2022-04-22 09:30:23 -0500

    fix: update url

--- inst_shellcheck.sh
+++ inst_shellcheck.sh
@@ -5,7 +5,7 @@ dest="$HOME/code/github.com/koalaman/shellcheck"
 if [ ! -f "$HOME/bin/shellcheck" ]; then
   echo " * Installing shellcheck"
   if [ ! -d "$dest" ]; then
-    git clone git://github.com/koalaman/shellcheck.git "$dest"
+    git clone git@github.com:koalaman/shellcheck.git "$dest"
   elif [ -d "$dest/.git" ]; then
     cd "$dest"
     git pull origin master
