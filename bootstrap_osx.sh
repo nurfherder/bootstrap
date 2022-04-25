18a91848821a1553c668fca7520b014fca393480
nurfherder23 <nurfherder23@gmail.com>
2022-04-25 10:29:44 -0500

    feat: update to run new scripts

--- bootstrap_osx.sh
+++ bootstrap_osx.sh
@@ -19,59 +19,76 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # microsoft remote desktop
 
 #==============================================================================
+# Install My Favorite Fonts
+#==============================================================================
+./inst_fonts.sh
+
+#==============================================================================
 # Install Homebrew (see http://brew.sh and http://caskroom.io)
 #==============================================================================
 # Install homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+./inst_homebrew.sh
 
 # Make sure everything is up to date
 brew update
 brew upgrade
 
-# Install Favorites
-brew install \
-caskroom/cask/brew-cask \
-ack \
-tmux \
-git \
-macvim \
-python \
-wget \
-zsh \
-dos2unix \
-pstree \
-screenresolution
+# Install Modern Dev Tools
+brew bundle --file=- <<-EOF
+brew "git"
+brew "wget"
+brew "tmux"
+brew "jq"
+brew "ack"
+brew "python"
+brew "dos2unix"
+brew "pstree"
+cask "visual-studio-code"
+EOF
 
 # Install Casks (non-admin)
-brew cask install \
-the-unarchiver \
-firefox \
-chrome \
-gimp \
-inkscape \
-hipchat \
-hammerspoon \
-keycastr \
-atom \
-menumeters
+
+# Utilities
+brew bundle --file=- <<-EOF
+brew "mas"
+cask "the-unarchiver"
+cask "hammerspoon"
+cask "menumeters"
+cask "keepassxc"
+#cask "keycastr"
+EOF
+
+# More browsers
+brew bundle --file=- <<-EOF
+cask "firefox"
+cask "google-chrome"
+cask "brave-browser"
+EOF
+
+# Graphics
+brew bundle --file=- <<-EOF
+cask "gimp"
+cask "inkscape"
+cask "blender"
+EOF
 
 # Install Casks (admin)
-brew cask install \
-virtualbox \
-xquartz \
-karabiner \
-seil
+#brew --cask install \
+#virtualbox \
+#xquartz \
+#karabiner \
+#seil
 
 
 #==============================================================================
 # Set system/application defaults
 #==============================================================================
-. osx/set-ActivityMonitor.sh
-. osx/set-AddressBook.sh
-. osx/set-AppStore.sh
-. osx/set-Chrome.sh
-. osx/set-DiskUtility.sh
-. osx/set-Finder.sh
+#. osx/set-ActivityMonitor.sh
+#. osx/set-AddressBook.sh
+#. osx/set-AppStore.sh
+#. osx/set-Chrome.sh
+#. osx/set-DiskUtility.sh
+#. osx/set-Finder.sh
 #. osx/set-GPGMail_2.sh
 #. osx/set-Global-bluetooth_headphones.sh
 #. osx/set-Global-hostname.sh
@@ -80,8 +97,8 @@ seil
 . osx/set-Global-trackpad_keyboard_mouse.sh
 . osx/set-Global-ui.sh
 . osx/set-Hammerspoon.sh
-. osx/set-Mail.sh
-. osx/set-Messages.sh
+#. osx/set-Mail.sh
+#. osx/set-Messages.sh
 #. osx/set-Opera.sh
 . osx/set-Safari.sh
 #. osx/set-SizeUp.sh
@@ -92,9 +109,9 @@ seil
 #. osx/set-TimeMachine.sh
 #. osx/set-Transmission.sh
 #. osx/set-Twitter.sh
-. osx/set-iCal.sh
+#. osx/set-iCal.sh
 #. osx/set-iTerm.sh
-. osx/set-iTunes.sh
+#. osx/set-iTunes.sh
 
 
 #==============================================================================
@@ -105,10 +122,10 @@ seil
 #==============================================================================
 # Kill affected applications                                                  #
 #==============================================================================
-for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
-	"Dock" "Finder" "Google Chrome" "Google Chrome Canary" "Mail" "Messages" \
-	"Opera" "Safari" "SizeUp" "Spectacle" "SystemUIServer" "Terminal" \
-	"Transmission" "Twitter" "iCal"; do
-	killall "${app}" > /dev/null 2>&1
-done
+#for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
+#	"Dock" "Finder" "Google Chrome" "Google Chrome Canary" "Mail" "Messages" \
+#	"Opera" "Safari" "SizeUp" "Spectacle" "SystemUIServer" "Terminal" \
+#	"Transmission" "Twitter" "iCal"; do
+#	killall "${app}" > /dev/null 2>&1
+#done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
